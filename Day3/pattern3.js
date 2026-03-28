8852b32d2fbc66240d43c7563c1b756ce40f5a6c
sagarksp <kspsagar01@gmail.com>
2026-03-28 15:23:30 +0530

    Pattern 3: Print a Right-Angled Number Triangle

---
+++ Day3/pattern3.js
@@ -0,0 +1,28 @@
+/*
+
+Pattern 3: Print a Right-Angled Number Triangle
+Write a program that prints a right-angled triangle of numbers of heightn.
+
+Output
+1
+1 2
+1 2 3
+1 2 3 4
+
+*/
+
+function numbers(n){
+    
+    for(i=0; i<n; i++){
+
+        let row = " "
+
+        for(j=1;j<=i; j++){
+            row = row + j;
+        }
+        console.log(row)
+    }
+
+}
+
+numbers(5)

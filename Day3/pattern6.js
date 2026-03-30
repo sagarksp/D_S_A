c10a9231c32cbf2523309b4a54ac0b4f3b02604f
sagarksp <kspsagar01@gmail.com>
2026-03-30 15:25:04 +0530

    Pattern 5: Print a Reverse Right-Angled Triangle of Increasing Numbers

---
+++ Day3/pattern6.js
@@ -0,0 +1,27 @@
+/*
+
+Pattern 6: Print a Right-Aligned Right-Angled Triangle of Stars
+Write a program that prints a right-aligned triangle of stars increasing row by row, with leading spaces for alignment.
+
+Output
+      *
+    * *
+  * * *
+* * * *
+
+*/
+
+
+function pattern6 (n){
+
+    for(i=0 ; i <n ; i++ )
+    {
+ 
+        console.log(" * ")
+ 
+    }
+
+
+}
+
+//
